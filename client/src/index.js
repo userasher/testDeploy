3eb29e04bc6c4859a07152da925cac9d28bb8094
userasher <ardange_b20@ee.vjti.ac.in>
2023-07-25 20:14:45 +0530

    fullfinaltobe deployed

--- client/src/index.js
+++ client/src/index.js
@@ -16,6 +16,13 @@ root.render(
   </Provider>
 );
 
+if (process.env.NODE_ENV !== "development") {
+  console.log = () => {};
+  console.debug = () => {};
+  console.info = () => {};
+  console.warn = () => {};
+}
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
